98ef47b1e58cfd462c122e1b8c8852248cb856eb
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-03-27 14:10:39 -0300

    validando entrada de input

--- Js/script.js
+++ Js/script.js
@@ -1,4 +1,7 @@
-window.addEventListener('load', () =>{
-  console.log('Conectado/*  */');
-  
+const amount = document.getElementById('amount') 
+
+amount.addEventListener('input', () =>{
+  const hasCharactersRegex = /\D+/g
+  amount.value = amount.value.replace(hasCharactersRegex, "")
+
 })
