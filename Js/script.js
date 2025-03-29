d26e9d86c7ae0c5fcb1fbbee0bfe2cfbb2a07966
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-03-29 12:32:37 -0300

    funcao de submit do formulario

--- Js/script.js
+++ Js/script.js
@@ -1,17 +1,44 @@
 const form = document.querySelector('form')
-const amount = document.getElementById('amount') 
+const amount = document.getElementById('amount')
 const currency = document.getElementById('currency')
+const footer = document.querySelector('footer')
+const description = document.getElementById('description')
 
-amount.addEventListener('input', () =>{
+
+const USD = 5.76
+const EUR = 6.24
+const GPB = 7.45
+
+amount.addEventListener('input', () => {
   const hasCharactersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharactersRegex, "")
 })
 
-form.onsubmit = (e) =>{
+form.onsubmit = (e) => {
   e.preventDefault()
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GPB, "£")
+      break;
+  }
+}
 
-  console.log(currency.value);
-  
-  
+function convertCurrency(amount, price, symbol) {
+  try {
+    description.textContent = `${symbol} 1 = ${price} `
+    footer.classList.add('show-result')
+
+  } catch (err) {
+    footer.classList.remove('show-result')
+    console.log(err);
+    alert("Não foi possível converter. Tente novamente")
+
+  }
 }
 
